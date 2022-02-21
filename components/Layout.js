0a7524b3226ff35bceb85f5f58417c7abd91a939
Salman Farsy <75604210+salmanfarsy@users.noreply.github.com>
2022-02-21 21:16:29 +0600

    Adding timetable component

--- components/Layout.js
+++ components/Layout.js
@@ -4,15 +4,24 @@ import Banner from './Banner';
 import Features from './Features';
 import Footer from './Footer';
 import Slider from './slider';
+<<<<<<< HEAD
+import TimeTable from './TimeTable';
+=======
+>>>>>>> aab4206b841b678d6f392b85d29fc13ab1642cf2
 
 const Layout = () => {
     return (
         <>
             <Banner />
             <Features />
+<<<<<<< HEAD
+            <About/>
+            <TimeTable/>
+=======
 
             <About/>
 
+>>>>>>> aab4206b841b678d6f392b85d29fc13ab1642cf2
             <Slider/>
             <Footer />
         </>
