c417bba1b5cb74e658f8def7b10cc1674cf38337
Salman Farsy <75604210+salmanfarsy@users.noreply.github.com>
2022-02-21 21:38:24 +0600

    merge the time table file (#3)
    * done with the slider section
    
    * done with the about section
    
    * Adding timetable component
    
    Co-authored-by: NurulIslam-EEE <85768783+NurulIslam-EEE@users.noreply.github.com>

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,13 @@
+
 import React from 'react';
 import About from './About';
 import Banner from './Banner';
 import Features from './Features';
 import Footer from './Footer';
 import Slider from './slider';
+
+import TimeTable from './TimeTable';
+
 
 const Layout = () => {
     return (
@@ -12,11 +16,17 @@ const Layout = () => {
             <Features />
 
             <About/>
+            <TimeTable/>
+
+
+            <About/>
+
 
             <Slider/>
             <Footer />
         </>
     );
 };
+
 
 export default Layout;
