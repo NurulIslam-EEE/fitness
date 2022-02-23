2ee074656df61bec38f86306c9f34483bc1fefaf
NurulIslam-EEE <ni163034@gmail.com>
2022-02-23 22:57:14 +0600

    feature section update

--- components/Features.js
+++ components/Features.js
@@ -5,69 +5,73 @@ import styles from "../styles/Features.module.css";
 const Features = () => {
     return (
         <>
-            <div>
-                <div className={styles.title}>
-                    <div>
-                        <p className={styles.box}></p>
-                        <p> WHY CHOOSE US?</p>
+            <div className={styles.container}>
+                <div>
 
+
+                    <div className={styles.title}>
+                        <div>
+                            <p className={styles.box}></p>
+                            <p> WHY CHOOSE US?</p>
+
+                        </div>
+
+                        <h1>OUR KEY FEATURES SEE SOME</h1>
                     </div>
 
-                    <h1>OUR KEY FEATURES SEE SOME</h1>
-                </div>
+                    <div className={styles.featureContainer}>
+                        <div className={styles.feature} >
+                            <div className={styles.img}>
+                                <Image src="/img/01.jpg" width="300" height="300" alt='' />
+                            </div>
 
-                <div className={styles.featureContainer}>
-                    <div className={styles.feature} >
-                        <div className={styles.img}>
-                            <Image src="/img/01.jpg" width="260" height="260" alt='' />
-                        </div>
+                            <div className={styles.bar}>
 
-                        <div className={styles.bar}>
+                            </div>
+                            <div>
+                                <h3>GET IN THE GROOVE</h3>
+                                <p>Use a past defeat as a motivator.</p>
+                                <p>Remind yourself you have.</p>
+                            </div>
+                        </div>
+                        <div className={styles.feature} >
+                            <div className={styles.img}>
+                                <Image src="/img/02.jpg" width="300" height="300" alt='' />
+                            </div>
 
-                        </div>
-                        <div>
-                            <h3>GET IN THE GROOVE</h3>
-                            <p>Use a past defeat as a motivator.</p>
-                            <p>Remind yourself you have.</p>
-                        </div>
-                    </div>
-                    <div className={styles.feature} >
-                        <div className={styles.img}>
-                            <Image src="/img/02.jpg" width="260" height="260" alt='' />
-                        </div>
+                            <div className={styles.bar}>
 
-                        <div className={styles.bar}>
+                            </div>
+                            <div>
+                                <h3>GET FIT DON’T QUIT</h3>
+                                <p>Success isn’t really that difficult. There is a significant?.</p>
+                            </div>
+                        </div>
+                        <div className={styles.feature} >
+                            <div className={styles.img} >
+                                <Image src="/img/03.jpg" width="300" height="300" alt='' />
+                            </div>
 
-                        </div>
-                        <div>
-                            <h3>GET FIT DON’T QUIT</h3>
-                            <p>Success isn’t really that difficult. There is a significant?.</p>
-                        </div>
-                    </div>
-                    <div className={styles.feature} >
-                        <div className={styles.img} >
-                            <Image src="/img/03.jpg" width="260" height="260" alt='' />
-                        </div>
+                            <div className={styles.bar}>
 
-                        <div className={styles.bar}>
+                            </div>
+                            <div>
+                                <h3>WORK YOUR BUTT OFF</h3>
+                                <p>For those of you who are serious about having more</p>
+                            </div>
+                        </div>
+                        <div className={styles.feature} >
+                            <div className={styles.img}>
+                                <Image src="/img/04.jpg" width="300" height="300" alt='' />
+                            </div>
 
-                        </div>
-                        <div>
-                            <h3>WORK YOUR BUTT OFF</h3>
-                            <p>For those of you who are serious about having more</p>
-                        </div>
-                    </div>
-                    <div className={styles.feature} >
-                        <div className={styles.img}>
-                            <Image src="/img/04.jpg" width="260" height="260" alt='' />
-                        </div>
+                            <div className={styles.bar}>
 
-                        <div className={styles.bar}>
-
-                        </div>
-                        <div>
-                            <h3>IT’S MORE THAN A GAME</h3>
-                            <p>For those of you who are serious about having more.</p>
+                            </div>
+                            <div>
+                                <h3>IT’S MORE THAN A GAME</h3>
+                                <p>For those of you who are serious about having more.</p>
+                            </div>
                         </div>
                     </div>
                 </div>
