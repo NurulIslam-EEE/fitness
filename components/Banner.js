6d439e7be8f5cb044b4b7fa81c2ef872dbf661de
NurulIslam-EEE <ni163034@gmail.com>
2022-02-25 14:45:59 +0600

    banner update

--- components/Banner.js
+++ components/Banner.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from '../styles/Banner.module.css';
 import { faFacebookF, faInstagram, faTwitter, faPinterest } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -8,6 +8,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 
 const Banner = () => {
+    const [bullet, setBullet] = useState(1);
+
     return (
         <div className={styles.banner}>
             <div className={styles.bannerSocialTitle}>
@@ -17,14 +19,21 @@ const Banner = () => {
 
 
 
-                        <div className={styles.bannerTitle}>
+                        {bullet === 1 ? <div className={styles.bannerTitle}>
 
                             <h3>Trainer in gym gives you training how to be fit </h3>
                             <h1>LOVE </h1> <h1><span>YOUR BODY.</span> </h1>
                             <h3>DO NOT LOSE TRACK OF YOUR GOAL DUE TO HEALTH ISSUES.</h3>
 
 
-                        </div>
+                        </div> : <div className={styles.bannerTitle}>
+
+                            <h3>Nothing tastes as good as being fit feels. </h3>
+                            <h1>MAKING </h1> <h1><span>FITNESS FUN.</span> </h1>
+                            <h3>Success never comes to those who quit..</h3>
+
+
+                        </div>}
                     </div>
                 </div>
                 <div className={styles.socialContainer}>
@@ -39,6 +48,10 @@ const Banner = () => {
                     </div>
 
                     <div className={styles.bar}></div>
+                </div>
+                <div className={styles.bullet}>
+                    <div className={bullet === 1 ? styles.whiteColor : styles.redColor} onClick={() => setBullet(1)}></div>
+                    <div className={bullet === 2 ? styles.whiteColor : styles.redColor} onClick={() => setBullet(2)}></div>
                 </div>
             </div>
 
